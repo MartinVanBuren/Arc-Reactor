68af96bc09b2afcc054c629f3308affade2d8fe0
Robert Kennedy <helmet0012@gmail.com>
2017-10-23 22:20:33 -0400

    Added empty arduino project with built in setup delay to allow the NeoPixels to receive proper power before initializing them.

---
+++ arc-reactor/arc-reactor.ino
@@ -0,0 +1,13 @@
+#include <Adafruit_NeoPixel.h>
+
+void setup() {
+  // Give LEDs time to receive power before setup
+  delay(250);
+
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
