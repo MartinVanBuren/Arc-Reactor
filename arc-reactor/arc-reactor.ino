8cb3600e279e8d19a59ab00e2fd92ea9bfb7ccd8
Robert Kennedy <robert076kennedy@gmail.com>
2018-01-22 02:14:28 -0500

    Completed the project.

--- arc-reactor/arc-reactor.ino
+++ arc-reactor/arc-reactor.ino
@@ -1,13 +1,19 @@
 #include <Adafruit_NeoPixel.h>
 
+#define PIN             1
+#define NUMPIXELS       31
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+uint32_t arcBlue = pixels.Color(5, 5, 255);
+
 void setup() {
   // Give LEDs time to receive power before setup
-  delay(250);
-
-  
+  pixels.begin();
+  for(int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, arcBlue);
+  }
+  pixels.setBrightness(255);
+  pixels.show();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+void loop() {}
